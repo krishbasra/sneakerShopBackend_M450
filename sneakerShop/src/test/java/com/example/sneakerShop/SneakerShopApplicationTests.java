1b999856bb720c21fc6ac31895ab378395d1178a
krishbasra <krish.basra@noseryoung.ch>
2023-11-02 14:49:15 +0100

    add client tests

---
+++ sneakerShop/src/test/java/com/example/sneakerShop/SneakerShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sneakerShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SneakerShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
